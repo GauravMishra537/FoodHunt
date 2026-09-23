267309b70aecc1815b8152b4fb52593f6d84cee7
GauravMishra537 <gauravmishra277401@gmail.com>
2026-09-23 18:26:08 +0530

    Updated delivery time in card

--- src/componenets/RestaurantCard.js
+++ src/componenets/RestaurantCard.js
@@ -2,7 +2,7 @@ import { CDN_URL } from "../utils/constants";
 
 const ResturantCard=(props)=>{
     const {resData}=props;
-    const { cloudinaryImageId,name,cuisines,avgRating,costForTwo,deliveryTime}=resData?.info;
+    const { cloudinaryImageId,name,cuisines,avgRating,costForTwo,  sla: { slaString } }=resData?.info;
     return(
         <div className="res-card" >
             <img
@@ -12,7 +12,7 @@ const ResturantCard=(props)=>{
             <h5>{cuisines.join(",")}</h5>
             <h5>{avgRating}</h5>
            <h5>{costForTwo }</h5>
-           <h5>{deliveryTime}</h5>
+           <h5>{slaString}</h5>
 
         </div>
     )
